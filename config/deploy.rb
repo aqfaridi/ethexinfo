cbf6759cba2f0f93f846d30d77ade0e203c60820
Abdul Qadir Faridi <aqfaridi@gmail.com>
2017-03-22 01:15:53 +0530

    add updated gemfile.lock to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:aqfaridi/ethexinfo.git'
 set :branch, :master
 set :deploy_to, '/home/deploy/ethexinfo'
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml config/local_env.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
